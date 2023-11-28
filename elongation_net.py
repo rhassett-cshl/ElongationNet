9b2dd2e36b3d098760f0aff920886b6b1c27e1d2
Rebecca Hassett <hassett@cshl.edu>
2023-11-28 13:29:55 -0500

    Convert notebook to python script

---
+++ elongation_net.py
@@ -0,0 +1,390 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.utils.data as td
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+
+froot = './data/k562_epft_chr22_norm'
+
+df = pd.read_csv(froot + ".csv")
+
+
+# In[2]:
+
+
+print(df.head())
+
+
+# In[3]:
+
+
+column_names = np.array(df.columns)
+feature_names = column_names[6:-2]#[6:-7]
+num_features = len(feature_names)
+#nucleotides = column_names[-6:-2]
+print(feature_names)
+#print(nucleotides)
+num_samples = df.shape[0]
+
+# process read counts
+X_ji = df['score'].values
+
+# process GLM simulated elongation rates
+Z_ji = df['zeta'].values
+
+
+# In[4]:
+
+
+print("Number of Samples: " + str(num_samples))
+
+
+# In[5]:
+
+
+print("Number of Features: " + str(num_features))
+
+
+# In[6]:
+
+
+#Y_ji is a list of samples containing lists of their feature values
+    # [   
+    #   sample_1: [feat_1, feat_2,...,feat_n],
+    #   sample_2: [feat_1, feat_2,...,feat_n],
+    # ]
+
+Y_ji = df.iloc[:, 6:-2].values#df.iloc[:, 6:-7].values
+
+
+# In[7]:
+
+
+C_j = df['lambda_alphaj'].values
+
+
+# In[8]:
+
+
+cuda_available = torch.cuda.is_available()
+print("CUDA (GPU support) is available:", cuda_available)
+num_gpus = torch.cuda.device_count()
+print("Number of GPUs available:", num_gpus)
+
+
+# In[9]:
+
+
+from torch.utils.data import Dataset, DataLoader
+
+class CustomDataset(Dataset):
+    def __init__(self, Y_ji, X_ji, C_j, Z_ji):
+        self.Y_ji = Y_ji
+        self.X_ji = X_ji
+        self.C_j = C_j
+        self.Z_ji = Z_ji
+
+    def __len__(self):
+        return len(self.Y_ji)
+
+    def __getitem__(self, idx):
+        return {
+            'Y_ji':  torch.tensor(self.Y_ji[idx], dtype=torch.float32),
+            'X_ji': torch.tensor(self.X_ji[idx], dtype=torch.float32),
+            'C_j': torch.tensor(self.C_j[idx], dtype=torch.float32),
+            'Z_ji': torch.tensor(self.Z_ji[idx], dtype=torch.float32)
+        }
+
+
+# In[10]:
+
+
+dataset = CustomDataset(Y_ji, X_ji, C_j, Z_ji)
+
+train_percent = 0.8
+val_percent = 0.1
+test_percent = 0.1
+train_size = int(train_percent * num_samples)
+val_size = int(val_percent * num_samples)
+test_size = num_samples - train_size - val_size
+trnset, valset, tstset = td.random_split(dataset, [train_size, val_size, test_size])
+
+batch_size = 64
+
+
+# In[11]:
+
+
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = nn.Linear(num_features, 1)
+
+#class CNN(nn.Module):
+#    def __init__(self):
+#        super(CNN, self).__init__()
+#        self.conv1 = nn.Conv1d(in_channels=1, out_channels=64, kernel_size=7, stride=1, padding=1)
+#        self.pool = nn.MaxPool1d(2, 2)
+#        self.fc = nn.Linear(64, 1)  
+
+#    def forward(self, x):
+#        x = x.unsqueeze(1)
+#        x = self.pool(F.relu(self.conv1(x)))
+#        x = torch.flatten(x, 1)
+#        x = self.fc(x)
+#        return x
+    
+#model = CNN()
+
+if cuda_available:
+    if num_gpus > 1:
+        print("Using", num_gpus, "GPUs")
+        model = torch.nn.DataParallel(model)
+    model = model.to('cuda')
+
+print(model)
+
+arr = torch.randn((batch_size,num_features)).to(device)
+print(model(arr).shape)
+nparm = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print("Number of parameters: " + str(nparm))
+
+first_param_device = next(model.parameters()).device
+print("Model is on device:", first_param_device)
+
+
+# In[12]:
+
+
+class CustomLoss(nn.Module):
+    def __init__(self):
+        super(CustomLoss, self).__init__()
+
+    def forward(self, X_ji, C_j, Z_ji):
+        #print(Z_ji.shape)
+        epsilon = 1e-8
+        clipped_Z_ji = torch.clamp(Z_ji, epsilon)
+        loss = X_ji * torch.log(clipped_Z_ji) + C_j * torch.exp(-clipped_Z_ji)
+        return (loss).mean()
+
+
+# In[13]:
+
+
+import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+num_epochs = 30
+
+loss_hist_train = [0] * num_epochs
+loss_hist_valid = [0] * num_epochs
+
+loss_fn = CustomLoss()
+
+for epoch in range(num_epochs):
+    print(f'Epoch {epoch+1}')
+    model.train()
+    trndl = DataLoader(trnset, batch_size=batch_size, num_workers=7, shuffle=True, pin_memory=True)
+    for i, batch in enumerate(trndl):
+        optimizer.zero_grad()
+        Y_ji_batch = batch['Y_ji'].to(device)
+        X_ji_batch = batch['X_ji'].to(device)
+        C_j_batch = batch['C_j'].to(device)
+        outputs = model(Y_ji_batch)
+        loss = loss_fn(X_ji_batch, C_j_batch, outputs)
+        loss.backward()
+        optimizer.step()
+        loss_hist_train[epoch] += loss.item()
+    loss_hist_train[epoch] /= len(trndl)
+    del trndl
+    
+    model.eval()
+    valdl = DataLoader(valset, batch_size=batch_size, num_workers=7, shuffle=False, pin_memory=True)
+    with torch.no_grad():
+        for batch in valdl:
+            Y_ji_batch = batch['Y_ji'].to(device)
+            X_ji_batch = batch['X_ji'].to(device)
+            C_j_batch = batch['C_j'].to(device)
+            outputs = model(Y_ji_batch)
+            loss = loss_fn(X_ji_batch, C_j_batch, outputs)
+            loss_hist_valid[epoch] += loss.item()
+        loss_hist_valid[epoch] /= len(valdl)
+    del valdl
+    
+    print(f'Epoch {epoch+1} trn_loss: '
+          f'{loss_hist_train[epoch]:.4f} val_loss: '
+          f'{loss_hist_valid[epoch]:.4f}')
+
+
+# In[14]:
+
+
+weights = model.weight.data.cpu().numpy()
+bias = model.bias.data.cpu().numpy()
+
+combined = ', '.join([f'"{s}": {f}' for s, f in zip(feature_names, weights[0])])
+print(combined)
+
+
+# In[112]:
+
+
+# GLM K
+
+# ctcf: -0.008355491
+# sj5: -0.114737868
+# sj3: -0.181587543
+# dms: -0.080544917
+# rpts: 0.063179057
+
+# gc: -0.240107566
+# histone: -0.043173335
+
+
+# In[15]:
+
+
+from datetime import datetime
+
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+filename = f"models/Elongation_Model_{timestamp}.pth"
+torch.save(model.state_dict(), filename)
+
+
+# In[24]:
+
+
+"""
+model = Model(num_features)
+model.load_state_dict(torch.load("models/Elongation_Model.pth"))
+cuda_available = torch.cuda.is_available()
+print("CUDA (GPU support) is available:", cuda_available)
+num_gpus = torch.cuda.device_count()
+print("Number of GPUs available:", num_gpus)
+if cuda_available:
+    if num_gpus > 1:
+        print("Using", num_gpus, "GPUs")
+        model = torch.nn.DataParallel(model)
+    model = model.to('cuda')
+
+first_param_device = next(model.parameters()).device
+print("Model is on device:", first_param_device)
+"""
+
+
+# In[16]:
+
+
+from matplotlib import pyplot as plt
+epochs = range(1, len(loss_hist_train) + 1)
+plt.plot(epochs, loss_hist_train, label='train_loss')
+plt.plot(epochs, loss_hist_valid, label='valid_loss')
+
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show 
+
+
+# In[17]:
+
+
+def plot_data(simulated_zeta, predicted_zeta):
+    indices = range(len(simulated_zeta))
+    
+    fig, ax = plt.subplots(figsize=(10, 5))
+    
+    ax.plot(indices, predicted_zeta, color='blue', alpha=0.4)
+    ax.plot(indices, simulated_zeta, color='orange', alpha=0.4)
+    
+    ax.scatter(indices, predicted_zeta, color='blue', label='Predicted Zeta')
+    ax.scatter(indices, simulated_zeta, color='orange', label='Simulated Zeta')
+    
+    ax.set_title('Predicted vs Simulated Elongation Rate')
+    ax.set_xlabel('Index')
+    ax.set_ylabel('Elongation Rate')
+    ax.legend()
+
+    plt.show()
+
+
+# In[18]:
+
+
+tstdl = DataLoader(tstset, batch_size=batch_size, num_workers=0, shuffle=False, pin_memory=True)
+
+
+# In[19]:
+
+
+model.eval()
+
+data_iter = iter(tstdl)
+for i in range(0, 4):
+    inputs = next(data_iter) 
+    print("number of samples: " + str(len(inputs)))
+
+    with torch.no_grad():
+        y_inputs = inputs['Y_ji'].to(device)
+        outputs = model(y_inputs)
+
+    simulated_zeta = inputs['Z_ji']
+    # convert log(Z) outputs to Z
+    predicted_zeta = torch.exp(outputs.cpu().squeeze())
+    print("predicted zeta:")
+    print(predicted_zeta)
+
+    print("simulated zeta:")
+    print(simulated_zeta)
+
+    plot_data(simulated_zeta, predicted_zeta)
+
+
+# In[20]:
+
+
+import torch.nn.functional as F
+
+predicted_zeta = []
+simulated_zeta = []
+with torch.no_grad():
+    for batch in tstdl:
+        y_inputs = batch['Y_ji'].to(device)
+        outputs = model(y_inputs)
+        # convert log(Z) outputs to Z
+        predicted_zeta.append(torch.exp(outputs.cpu()))
+        simulated_zeta.append(batch['Z_ji'])
+
+predicted_zeta = torch.cat(predicted_zeta, dim=0)
+simulated_zeta = torch.cat(simulated_zeta, dim=0)
+mae = F.l1_loss(predicted_zeta.squeeze(), simulated_zeta)
+mse = F.mse_loss(predicted_zeta.squeeze(), simulated_zeta)
+
+print(f"Mean Absolute Error: {mae.item():.4f}")
+print(f"Mean Squared Error: {mse.item():.4f}")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
