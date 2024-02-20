e45402404a02913424facf1e366db7bc4c405348
Rebecca Hassett <hassett@cshl.edu>
2024-02-19 21:51:38 -0500

    add qsub submit job script

---
+++ submit_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -cwd
+#$ -V
+#$ -N elongation_net
+#$ -o output.txt
+#$ -e errors.txt
+#$ -S /bin/bash
+#$ -l h_rt=10:00:00
+#$ -l h_vmem=500G
+#$ -l gpu=1
+#$ -pe threads 8
+
+python -u elongation_epAllmer_net.py
