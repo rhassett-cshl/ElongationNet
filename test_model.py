0b267e14298bff51df2ce0c5cf39e53ea7cc7788
Rebecca Hassett <hassett@cshl.edu>
2024-02-26 15:26:56 -0500

    Add file for training model for analysis using pre-set data split

--- test_model.py
+++ test_model.py
@@ -44,57 +44,30 @@ def train_model(use_wandb, config_name, config):
     if config["train_use_sliding_window"]:
         train_window_size = config["train_window_size"]
     train_loader = setup_dataloader(train_data, feature_names, nucleotides, train_batch_size, config["train_use_sliding_window"], train_window_size)
-    
-    valid_window_size = None
-    if config["valid_use_sliding_window"]:
-        valid_window_size = config["valid_window_size"]
-    valid_loader = setup_dataloader(valid_data, feature_names, nucleotides, valid_batch_size, config["valid_use_sliding_window"], valid_window_size)
-    
+
     optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'], weight_decay=config['l2_lambda'])
         
     loss_fn = torch.jit.script(CustomLoss())
 
-    # for bucketing input
-    #loss_fn = torch.jit.script(BucketLoss())
-
     # track loss curves
     loss_neural_net_train = [0] * config["epochs"]
-    loss_neural_net_valid = [0] * config["epochs"]
-    loss_glm_valid = [0] * config["epochs"]
+    loss_glm_train = [0] * config["epochs"]
     
-    # scheduler to reduce learning rate by half when new validation loss > old validation loss
-    old_neural_net_valid_loss = float('inf')
+    # scheduler to reduce learning rate by half when new train loss > old train loss
+    old_neural_net_train_loss = float('inf')
     epochs_no_improve = 0
     scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=1, verbose=True)
 
     for epoch in range(config["epochs"]):
         print(f'Epoch {epoch+1}')
         
-        train_loss, glm_train_loss = train_epoch(model, train_loader, device, optimizer, loss_fn, config['l1_lambda'])
-        loss_neural_net_train[epoch] = train_loss
-        print(f"train loss: {train_loss: .5f}")
-        
-        valid_neural_net_loss, valid_glm_loss, neural_net_zeta, glm_zeta = valid_epoch(model, valid_loader, device, loss_fn)
-        loss_neural_net_valid[epoch] = valid_neural_net_loss
-        loss_glm_valid[epoch] = valid_glm_loss
-        print(f"valid neural net loss: {valid_neural_net_loss: .5f}")
-        print(f"valid glm loss: {valid_glm_loss: .5f}")
-        
-        # compute metrics
-        mae = F.l1_loss(neural_net_zeta.squeeze(), glm_zeta)
-        mse = F.mse_loss(neural_net_zeta.squeeze(), glm_zeta)
-        correlation_coefficient = np.corrcoef(glm_zeta, neural_net_zeta.squeeze())[0, 1]
-        print("Correlation Coefficient:", correlation_coefficient)
-        print(f"Mean Absolute Error: {mae.item():.4f}")
-        print(f"Mean Squared Error: {mse.item():.4f}")
-        
-        if use_wandb:
-            wandb.log({"epoch": epoch, "train_loss": train_loss, "valid_neural_net_loss": valid_neural_net_loss,
-           "valid_glm_loss": valid_glm_loss, "correlation_coefficient": correlation_coefficient,
-           "mae": mae, "mse": mse})
+        neural_net_train_loss, glm_train_loss = train_epoch(model, train_loader, device, optimizer, loss_fn, config['l1_lambda'])
+        loss_neural_net_train[epoch] = neural_net_train_loss
+        print(f"neural net train loss: {neural_net_train_loss: .5f}")
+        print(f"glm train loss: {glm_train_loss: .5f}")
         
         # early stopping
-        if valid_neural_net_loss < old_neural_net_valid_loss - increase_cut:
+        if neural_net_train_loss < old_neural_net_train_loss - increase_cut:
             epochs_no_improve = 0
         else:
             epochs_no_improve += 1
@@ -104,16 +77,16 @@ def train_model(use_wandb, config_name, config):
             break
         
         # reduce learning rate if new loss > old loss
-        if valid_neural_net_loss > old_neural_net_valid_loss:
+        if neural_net_train_loss > old_neural_net_train_loss:
             optimizer.param_groups[0]['lr'] *= 0.5
             print(f"Reduced learning rate to {optimizer.param_groups[0]['lr']}")
             
-        old_neural_net_valid_loss = valid_neural_net_loss
-        scheduler.step(valid_neural_net_loss)
+        old_neural_net_train_loss = neural_net_train_loss
+        scheduler.step(neural_net_train_loss)
         
     
     if not use_wandb:
         filename = f"./model_checkpoints/{config_name}.pth"
         torch.save(model.state_dict(), filename)
         
-    return model, loss_neural_net_train, loss_neural_net_valid, loss_glm_valid
+    return model, loss_neural_net_train
--- train_epoch.py
+++ train_epoch.py
@@ -7,11 +7,13 @@ import torch.optim as optim
 def train_epoch(model, loader, device, optimizer, loss_fn, l1_lambda):
     model.train()
     total_loss = 0
+    total_glm_loss = 0
     for idx, batch in enumerate(loader):
         optimizer.zero_grad(set_to_none=True)
         Y_ji_batch = batch['Y_ji'].to(device) 
         X_ji_batch = batch['X_ji'].to(device)
         N_ji_batch = batch['N_ji'].to(device) 
+        Z_ji_batch = batch['Z_ji'].to(device) 
         C_j_batch = batch['C_j'].to(device).unsqueeze(1)
         #Mask = batch['Mask'].to(device) # for bucketing input
         
@@ -21,6 +23,7 @@ def train_epoch(model, loader, device, optimizer, loss_fn, l1_lambda):
             outputs = model(Y_ji_batch, N_ji_batch)
 
         loss = loss_fn(X_ji_batch, C_j_batch, outputs)#, Mask)
+        glm_loss = loss_fn(X_ji_batch, C_j_batch, Z_ji_batch)
 
         if l1_lambda != 0:
             l1_norm = sum(torch.abs(p).sum() for p in model.parameters())
@@ -31,6 +34,8 @@ def train_epoch(model, loader, device, optimizer, loss_fn, l1_lambda):
         
         # calculate average loss across all batches
         total_loss += loss.item()
+        total_glm_loss += glm_loss.item()
     avg_train_loss = total_loss / len(loader)
+    avg_glm_train_loss = total_glm_loss / len(loader)
     
-    return avg_train_loss
+    return avg_train_loss, avg_glm_train_loss
