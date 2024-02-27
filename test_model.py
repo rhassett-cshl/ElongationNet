189cdbc54e4c2a3c7f9895e2c620de41ab7a8a61
Rebecca Hassett <hassett@cshl.edu>
2024-02-26 22:13:27 -0500

    Fix error in model test

--- test_model.py
+++ test_model.py
@@ -10,7 +10,7 @@ from load_model_checkpoint import load_model_checkpoint
 from data_processing.load_data import read_pickle, setup_dataloader
 from loss_debug import CustomLoss
 
-config_name = "cnn_2" # ep_seq_linear_1
+config_name = "ep_seq_linear_1"  #"cnn_2" # ep_seq_linear_1
 
 with open("./configs/" + config_name + ".json", 'r') as file:
     config = json.load(file)
@@ -40,7 +40,7 @@ test_dl = setup_dataloader(test_data, feature_names, nucleotides, test_batch_siz
 
 loss_fn = CustomLoss()
 
-csv_columns = ["Chr", "Start", "Strand", "GeneId", "Expected_X_ji", "C_j", "CNN_Zeta", "Loss", "GLM_Combined_Zeta", "Predicted_X_ji"]
+csv_columns = ["Chr", "Start", "Strand", "GeneId", "Expected_X_ji", "C_j", "Rho_ji", "CNN_Zeta", "Loss", "GLM_Combined_Zeta", "Predicted_X_ji"]
 csv_data = {col: [] for col in csv_columns}
 with torch.no_grad():
     for idx, batch in enumerate(test_dl):
@@ -60,10 +60,11 @@ with torch.no_grad():
 
         csv_data["Chr"].extend(batch["Chr"][0])
         csv_data["Start"].extend(batch["Start"][0].numpy())
+        csv_data["Rho_ji"].extend(rho_ji.squeeze().cpu().numpy())
         csv_data["CNN_Zeta"].extend(torch.exp(rho_ji.squeeze().cpu()).numpy())
         csv_data["GLM_Combined_Zeta"].extend(batch["Z_ji"][0].numpy())
         csv_data["Strand"].extend(batch["Strand"][0].numpy())
-        csv_data["Loss"].extend(loss_items.numpy())
+        csv_data["Loss"].extend(loss_items[0].numpy())
         csv_data["Expected_X_ji"].extend(batch["X_ji"][0].numpy())
 
         csv_data["C_j"].extend([batch["C_j"][0].numpy()] * batch_size)
